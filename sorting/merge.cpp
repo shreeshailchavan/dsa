b2001c72672f0432ebd9989beafd7cf013d6285e
shreeshailchavan <shreeshailchavan309@gmail.com>
2025-04-18 18:41:57 +0530

    inplace merge sort

--- sorting/merge.cpp
+++ sorting/merge.cpp
@@ -48,11 +48,56 @@ vector<int> merge_sort(vector<int> arr){
 }
 
 
+// inplace merge sort
+
+void inplace_merge(vector<int>& arr,int left,int mid,int right){
+    int low = left;
+    int high = mid+1;
+    vector<int> temp;
+
+    while(low <= mid && high <= right){
+        if(arr[low] < arr[high]){
+            temp.push_back(arr[low]);
+            low++;
+        }
+        else{
+            temp.push_back(arr[high]);
+            high++;
+        }
+    }
+
+    while(low <= mid){
+        temp.push_back(arr[low]);
+        low++;
+    }
+
+    while(high <= right){
+        temp.push_back(arr[high]);
+        high++;
+    }
+
+    for(int i=left;i<=right;i++){
+        arr[i] = temp[i-left];
+    }
+}
+
+void inplace_merge_sort(vector<int>& v,int left,int right){
+    if(left>=right)
+        return;
+    int mid = (left+right)/2;
+    inplace_merge_sort(v,left,mid);
+    inplace_merge_sort(v,mid+1,right);
+
+    inplace_merge(v,left,mid,right);
+}
+
+
 
 int main(){
     vector<int> v({3, 7, 6, -10, 15, 25, 55, -13});
-    vector<int> merged = merge_sort(v);
-    for(auto& it:merged){
+    // vector<int> merged = merge_sort(v);
+    inplace_merge_sort(v,0,v.size()-1);
+    for(auto& it:v){
         cout<<it<<" ";
     }
 
