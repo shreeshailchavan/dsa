a426ca4c6b3cd1c7ff9c1fc35aaef87df0f64e02
shreeshailchavan <shreeshailchavan309@gmail.com>
2025-04-22 23:04:38 +0530

    level order bfs with queue

--- trees/bfs.cpp
+++ trees/bfs.cpp
@@ -72,6 +72,38 @@ void inorder(Node* root){
     inorder(root->right);
 }
 
+
+vector<vector<int>> inorderWithQueue(Node* root){
+    vector<vector<int>> res;
+    queue<Node*> q;
+    q.push(root);
+    int currentLevel = 0;
+    while(!q.empty()){
+        int len = q.size();
+        res.push_back({});
+        for(int i=0;i<len;i++){
+            Node* node = q.front();
+            q.pop();
+            cout << "data->" << node->data<<"   ";
+            res[currentLevel].push_back(node->data);
+
+            if(node->left != nullptr){
+                q.push(node->left);
+            }
+
+            if(node->right != nullptr){
+                q.push(node->right);
+            }
+        }
+        currentLevel++;
+    }
+
+
+    return res;
+
+
+}
+
 int main(){
     Node* root = nullptr;
     addNode(root,10);
@@ -89,6 +121,15 @@ int main(){
         cout<<endl;
     }
 
+    // Inorder with queue
+    vector<vector<int>> result = inorderWithQueue(root);
+    for(auto it:result){
+        cout<<"[";
+        for(int i:it){
+            cout<<i<<" ";
+        }
+        cout << "]"<<endl;
+    }
 
 
 
